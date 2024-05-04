343b309dd51a3935c8a099922d5915a5102c246d
Roxana Muntian <roxana@fiveem.com>
2024-05-04 20:19:19 +0300

    Handle fetching the messages and display them.

--- components/Chat.vue
+++ components/Chat.vue
@@ -21,11 +21,32 @@
     </div>
 
     <!-- Messages -->
+    <div class="text-center text-2xl py-12" v-if="pending">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-12 h-12 mx-auto animate-spin"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+        />
+      </svg>
+    </div>
     <div
+    v-else
       class="flex flex-col gap-3.5 py-5 px-3 overflow-y-scroll max-h-[400px]"
     >
       <!-- To flip message use "flex-row-reverse" -->
-      <div v-for="message in messages" class="flex items-end gap-3" :class="{ 'flex-row-reverse': !message.isRoxana }">
+      <div
+        v-for="message in messages"
+        class="flex items-end gap-3"
+        :class="{ 'flex-row-reverse': !message.isRoxana }"
+      >
         <!-- Profile Image -->
         <img
           v-if="message.isRoxana"
@@ -59,4 +80,22 @@
 <script setup lang="ts">
 const messages = useMessages();
 const { customerInitials } = useCustomer();
+
+const { pending } = await useFetch("/api/message", {
+  lazy: true,
+  onResponse({ response }) {
+    const content = response._data.data[0].content[0];
+
+    if (content?.type == "text") {
+      messages.value.push({
+        name: "Roxana",
+        isRoxana: true,
+        message: content.text.value,
+        timestamp: new Date().toLocaleString([], {
+          timeStyle: "short",
+        }),
+      });
+    }
+  },
+});
 </script>
